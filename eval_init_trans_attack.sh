90275f849341f50700072b3e986304ab825fe78e
WinterCodeForEverything <1008377@mymail.sutd.edu.sg>
2025-04-06 11:29:24 +0800

    eval noise size

--- eval_init_trans_attack.sh
+++ eval_init_trans_attack.sh
@@ -1,7 +1,7 @@
 python pointllm/eval/eval_objaverse.py \
-    --data_path /mnt/ssd/liuchao/PointLLM/Objaverse_npy \
-    --anno_path /mnt/ssd/liuchao/PointLLM/PointLLM_brief_description_val_200_GT.json \
+    --data_path /mnt/ssd/liuchao/PointLLM/Objaverse_adv_npy_epsilon_0.02\
+    --anno_path /mnt/ssd/liuchao/PointLLM/PointLLM_brief_description_adv_200.json \
     --model_name RunsenXu/PointLLM_7B_v1.2 \
-    --output_dir ./result \
+    --output_dir ./result/noise_size_2_result \
     --task_type classification \
     --prompt_index 0
