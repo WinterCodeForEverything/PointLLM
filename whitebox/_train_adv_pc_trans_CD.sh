e0d020981d90d0e929855c1f8406c06171787954
WinterCodeForEverything <chao_liu@mymail.sutd.edu.sg>
2025-04-09 17:28:27 +0800

    fix CD bugs

--- whitebox/_train_adv_pc_trans_CD.sh
+++ whitebox/_train_adv_pc_trans_CD.sh
@@ -2,13 +2,14 @@
 
 
 # certain hyper-parameters can be modified based on user's preference
-CUDA_VISIBLE_DEVICES=1 \
+CUDA_VISIBLE_DEVICES=0 \
 python _train_adv_pc_trans_CD.py \
     --model_name 'RunsenXu/PointLLM_7B_v1.2' \
     --data_path '/mnt/ssd/liuchao/PointLLM/Objaverse_npy' \
     --ori_anno_path '/mnt/ssd/liuchao/PointLLM/PointLLM_brief_description_val_200_GT.json' \
     --adv_anno_path '/mnt/ssd/liuchao/PointLLM/PointLLM_brief_description_adv_200.json' \
-    --output_pc_path '/mnt/ssd/liuchao/PointLLM/Objaverse_adv_npy_chamfer_distance' \
+    --output_pc_path '/mnt/ssd/liuchao/PointLLM/Objaverse_adv_npy_CD_noise_size_5' \
+    --epsilon 0.05 \
     --alpha 0.001 \
     --gamma1 1.0 \
     --batch_size 16 \
@@ -17,6 +18,4 @@ python _train_adv_pc_trans_CD.py \
     --use_color \
     --wandb \
     --wandb_project_name 'PointLLM' \
-    --wandb_run_name 'eva_chamfer_distance_constrain' \
-
-    # --epsilon 0.05 \
+    --wandb_run_name 'eva_chamfer_CD_noise_size_5' \
